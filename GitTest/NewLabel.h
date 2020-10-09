515cabded4675febf00902a65f65727fc09c111c
jinzhenlu <805192369@qq.com>
2020-10-09 11:22:00 +0800

    new label

---
+++ GitTest/NewLabel.h
@@ -0,0 +1,17 @@
+//
+//  NewLabel.h
+//  GitTest
+//
+//  Created by Developer on 2020/10/9.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewLabel : UILabel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitTest/NewLabel.m
@@ -0,0 +1,21 @@
+//
+//  NewLabel.m
+//  GitTest
+//
+//  Created by Developer on 2020/10/9.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+#import "NewLabel.h"
+
+@implementation NewLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
